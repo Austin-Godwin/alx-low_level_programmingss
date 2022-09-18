a1b9e6dc4db6bbae9dda795ed128152b95760e67
Austin-Godwin <austingodwin18@gmail.com>
2022-09-17 20:48:21 -0700

    unction that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two interger
+ * @a: The first integer to be multipled
+ * @b: The second interger to be multipled with
+ * the first integer
+ *
+ * Return: The result of the multiplied integers
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
